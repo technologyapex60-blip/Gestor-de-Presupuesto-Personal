00de31a2678712f6a1463fe00dcf1e653801325e
Stiven <technologyapex60@gmail.com>
2026-06-13 02:35:37 -0400

    correcion de main

--- Gestor de Presupuesto Personal.API/Gestor de Presupuesto Personal.API/Data/DataStore.cs
+++
@@ -1,8 +0,0 @@
-﻿using Gestor_de_Presupuesto_Personal.API.Model;
-namespace Gestor_de_Presupuesto_Personal.API.Data;
-
-public static class DataStore
-{
-    public static List<Usuario> Usuarios { get; } = new();
-
-}
