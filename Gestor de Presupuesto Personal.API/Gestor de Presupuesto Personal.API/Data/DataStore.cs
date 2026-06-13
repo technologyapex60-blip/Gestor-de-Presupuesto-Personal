076473a1cd2381f29f9e70fd0ce28bfdaaba5550
Stiven <technologyapex60@gmail.com>
2026-06-13 01:52:44 -0400

    Create User

---
+++ Gestor de Presupuesto Personal.API/Gestor de Presupuesto Personal.API/Data/DataStore.cs
@@ -0,0 +1,8 @@
+﻿using Gestor_de_Presupuesto_Personal.API.Model;
+namespace Gestor_de_Presupuesto_Personal.API.Data;
+
+public static class DataStore
+{
+    public static List<Usuario> Usuarios { get; } = new();
+
+}
