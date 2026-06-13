6859ea34d2e9e2f90d9de6685940dd65d9a93818
Stiven <technologyapex60@gmail.com>
2026-06-13 10:38:08 -0400

    Creacion de los dto lista

---
+++ Gestor de Presupuesto Personal/Gestor de Presupuesto Personal.API/Model/Entities/CategoriaModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Gestor_de_Presupuesto_Personal.API.Model.Entities;
+
+public class Categoria
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = string.Empty;
+
+    public string Tipo { get; set; } = string.Empty;
+}
---
+++ Gestor de Presupuesto Personal/Gestor de Presupuesto Personal.API/Model/Entities/UsuarioModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Gestor_de_Presupuesto_Personal.API.Model.Entities;
+
+public class Usuario
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = string.Empty;
+
+    public string Correo { get; set; } = string.Empty;
+
+  
+
+
+
+}
